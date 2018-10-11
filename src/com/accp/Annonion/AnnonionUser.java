87ae08e4080ba1202bfbb2feb7a9f88454fe2bd6
merrybiao <787347862@qq.com>
2018-10-11 21:55:58 +0800

    测试github如何更新

--- src/com/accp/Annonion/AnnonionUser.java
+++ src/com/accp/Annonion/AnnonionUser.java
@@ -4,7 +4,11 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import com.accp.entity.User;
-
+/**
+ * 
+ * @author Administrator
+ *
+ */
 public interface AnnonionUser {
 	
 	@Select("select * from user where id = #{id}")
